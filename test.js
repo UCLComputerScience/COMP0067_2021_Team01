13cefbd6fff20415ab702671ca6926b4e3b37d79
Hu Weicong <zczlwh3@ucl.ac.uk>
2021-03-21 22:16:05 +0800

    2021.3.21

--- test.js
+++ test.js
@@ -52,7 +52,7 @@ web.post('/login', function(req, res){
         }
         else if (loginInfo[0].password === user.upassword) {
             if (loginInfo[0].userType === "lecturer")
-            res.redirect('/')
+            res.redirect('/lecturerHomepage')
         }
         else {
             res.redirect('/pwError')
@@ -61,7 +61,7 @@ web.post('/login', function(req, res){
     connection.end()
 })
 
-web.get('/', function(req, res){
+web.get('/lecturerHomepage', function(req, res){
     if (req.session.uname) {
         var uname = req.session.uname
     } else {
@@ -81,25 +81,13 @@ web.get('/', function(req, res){
         }
         req.session.allModules = allMod
         var allModules = req.session.allModules
-        var html = [
-            "Sorry lecturer, you haven't registered a module",
-            "Please go to ",
-            "Admin page ",
-            "to create your module"
-            ]
-        if (allModules.length ===0){
-            res.render('index.html', {
-                html: html,
-                modules: allModules,
-                uname: uname
-            })
-        } else {
-            res.render('index.html', {
-                html: '',
-                modules: allModules,
-                uname: uname
-            })
+        for (var i = 1; i <= allModules.length; i++) {
+            allModules[i-1].index = i;
         }
+        res.render('lecturerHomepage.html', {
+            uname: uname,
+            modules: allModules
+        })
     })
     // connection.end()
 })
@@ -121,7 +109,7 @@ web.get('/module', function(req, res){
     // query the data used in student table
     var queryStudentAverageScore = 'SELECT studentSPR, AVG(score) avgScore FROM `StudentFeedback` ' +
         'WHERE moduleCode = "'+selectedModuleID+'" GROUP BY studentSPR'
-    var queryStudentLastScore = 'SELECT studentSPR, score lastScore FROM StudentFeedback ' +
+    var queryStudentLastScore = 'SELECT studentSPR, score lastScore, DATE_FORMAT(date,"%Y-%m-%d") date, messageLecturer FROM StudentFeedback ' +
         'WHERE `moduleCode`="'+selectedModuleID+'" and (studentSPR, weekNumber) IN ' +
         '(SELECT studentSPR, MAX(weekNumber) FROM StudentFeedback WHERE `moduleCode`="'+selectedModuleID+'" GROUP BY studentSPR)'
     var queryStudentTeamProject = 'SELECT * FROM `ModStuTe` JOIN `Student` ON ModStuTe.studentSPR=Student.studentSPR ' +
@@ -137,17 +125,15 @@ web.get('/module', function(req, res){
         'WHERE `moduleCode`="'+selectedModuleID+'" GROUP BY ModStuTe.teamNumber '
     var queryGroupAverageScore = 'SELECT teamNumber, AVG(score) avgScore FROM `TeamFeedback` ' +
         'WHERE moduleCode = "'+selectedModuleID+'" GROUP BY teamNumber '
-    var queryGroupLastScore = 'SELECT teamNumber, score lastScore FROM TeamFeedback ' +
+    var queryGroupLastScore = 'SELECT teamNumber, score lastScore, DATE_FORMAT(date,"%Y-%m-%d") date, messageLecturer FROM TeamFeedback ' +
         'WHERE `moduleCode`="'+selectedModuleID+'" and (teamNumber, weekNumber) IN ' +
         '(SELECT teamNumber, MAX(weekNumber) FROM TeamFeedback WHERE `moduleCode`="'+selectedModuleID+'" GROUP BY teamNumber) '
-    var queryGroupProjectTA='SELECT * FROM `ModTeTA` ' +
-        'JOIN `ProjectInfo` ON (ModTeTA.teamNumber=ProjectInfo.teamNumber and ModTeTA.moduleCode=ProjectInfo.moduleCode) ' +
-        'JOIN `TA` ON (ModTeTA.taStudentSPR=TA.taStudentSPR) '
+    var queryGroupProjectTA='SELECT * FROM `ProjectInfo` JOIN `TA` ON (ProjectInfo.taStudentSPR=TA.taStudentSPR) '
     var queryAllGroupTable = queryGroupProjectTA +
-        ' JOIN (' + queryTeamMembers + ') AS TeamMembers ON (ModTeTA.teamNumber=TeamMembers.teamNumber) '+
-        'JOIN ('+queryGroupAverageScore+') AS gAvgScore ON (ModTeTA.teamNumber=gAvgScore.teamNumber) ' +
-        'JOIN ('+queryGroupLastScore+') AS gLastScore ON (ModTeTA.teamNumber=gLastScore.teamNumber) ' +
-        'WHERE ModTeTA.moduleCode="'+selectedModuleID+'" '
+        ' JOIN (' + queryTeamMembers + ') AS TeamMembers ON (ProjectInfo.teamNumber=TeamMembers.teamNumber) '+
+        'JOIN ('+queryGroupAverageScore+') AS gAvgScore ON (ProjectInfo.teamNumber=gAvgScore.teamNumber) ' +
+        'JOIN ('+queryGroupLastScore+') AS gLastScore ON (ProjectInfo.teamNumber=gLastScore.teamNumber) ' +
+        'WHERE ProjectInfo.moduleCode="'+selectedModuleID+'" '
 
     // query the data used in student need attention table
     var queryAttStudentTable = queryStudentTeamProject +
@@ -157,10 +143,10 @@ web.get('/module', function(req, res){
 
     // query the data used in group need attention table
     var queryAttGroupTable = queryGroupProjectTA +
-        ' JOIN (' + queryTeamMembers + ') AS TeamMembers ON (ModTeTA.teamNumber=TeamMembers.teamNumber) '+
-        'JOIN ('+queryGroupAverageScore+') AS gAvgScore ON (ModTeTA.teamNumber=gAvgScore.teamNumber) ' +
-        'JOIN ('+queryGroupLastScore+') AS gLastScore ON (ModTeTA.teamNumber=gLastScore.teamNumber) ' +
-        'WHERE ModTeTA.moduleCode="'+selectedModuleID+'" and gLastScore.lastScore < 4.1 '
+        ' JOIN (' + queryTeamMembers + ') AS TeamMembers ON (ProjectInfo.teamNumber=TeamMembers.teamNumber) '+
+        'JOIN ('+queryGroupAverageScore+') AS gAvgScore ON (ProjectInfo.teamNumber=gAvgScore.teamNumber) ' +
+        'JOIN ('+queryGroupLastScore+') AS gLastScore ON (ProjectInfo.teamNumber=gLastScore.teamNumber) ' +
+        'WHERE ProjectInfo.moduleCode="'+selectedModuleID+'" and gLastScore.lastScore < 4.1 '
 
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -239,7 +225,8 @@ web.get('/group', function(req, res){
     var queryContribution = 'SELECT weekNumber WN, group_concat(contribution Separator ", ") contributions FROM `StudentFeedback` ' +
         'JOIN `ModStuTe` ON (ModStuTe.studentSPR=StudentFeedback.studentSPR AND ModStuTe.moduleCode=StudentFeedback.moduleCode) ' +
         'WHERE ModStuTe.moduleCode="'+selectedModuleID+'" AND teamNumber="'+teamNumber+'" GROUP BY weekNumber'
-    var queryTeamFeedback = 'SELECT * FROM `TeamFeedback` JOIN ('+ queryContribution+') AS Con ON TeamFeedback.weekNumber=WN ' +
+    var queryTeamFeedback = 'SELECT weekNumber, score, contributions, writtenFeedback, messageLecturer, DATE_FORMAT(date,"%Y-%m-%d") date ' +
+        'FROM `TeamFeedback` JOIN ('+ queryContribution+') AS Con ON TeamFeedback.weekNumber=WN ' +
         'WHERE teamNumber="' + teamNumber + '" AND `moduleCode`="'+selectedModuleID+'"'
 
     var connection = mysql.createConnection({
@@ -303,7 +290,7 @@ web.get('/student', function(req, res){
         'WHERE ModStuTe.moduleCode="'+selectedModuleID+'" AND ModStuTe.studentSPR="'+studentSPR+'"'
 
     // query the feedback of the selected student
-    var queryStudentFeedback = 'SELECT * FROM studentFeedback WHERE studentSPR="'+studentSPR+'" AND moduleCode="'+selectedModuleID+'"'
+    var queryStudentFeedback = 'SELECT weekNumber, score, contribution, writtenFeedback, messageLecturer, DATE_FORMAT(date,"%Y-%m-%d") date FROM studentFeedback WHERE studentSPR="'+studentSPR+'" AND moduleCode="'+selectedModuleID+'"'
 
     var connection = mysql.createConnection({
         host: 'localhost',
@@ -390,21 +377,21 @@ web.post('/addModule', function(req, res){
         }
     }
 
-    // Insert into ModTeTA table
-    for (i = 1; i < groupingData.length; i++) {
-        var queryInsertModTeTA = 'INSERT INTO `ModTeTA`(`moduleCode`, `teamNumber`, `taStudentSPR`) ' +
-            'VALUES ("'+add.modID+'", "'+groupingData[i][0]+'", "'+groupingData[i][3]+'")'
-        connection.query(queryInsertModTeTA, function (error, results) {
-            if (error) {
-                console.log(error)
-            }
-        })
-    }
+    // // Insert into ModTeTA table
+    // for (i = 1; i < groupingData.length; i++) {
+    //     var queryInsertModTeTA = 'INSERT INTO `ModTeTA`(`moduleCode`, `teamNumber`, `taStudentSPR`) ' +
+    //         'VALUES ("'+add.modID+'", "'+groupingData[i][0]+'", "'+groupingData[i][3]+'")'
+    //     connection.query(queryInsertModTeTA, function (error, results) {
+    //         if (error) {
+    //             console.log(error)
+    //         }
+    //     })
+    // }
 
     // Insert into ProjectInfo table
     for (i = 1; i < groupingData.length; i++) {
-        var queryInsertProjectInfo = 'INSERT INTO `ProjectInfo`(`moduleCode`, `teamNumber`, `labCode`, `projectTitle`, `projectBrief`) ' +
-            'VALUES ("'+add.modID+'", "'+groupingData[i][0]+'","" , "'+groupingData[i][1]+'", "'+groupingData[i][2]+'")'
+        var queryInsertProjectInfo = 'INSERT INTO `ProjectInfo`(`moduleCode`, `teamNumber`, `taStudentSPR`, `labCode`, `projectTitle`, `projectBrief`) ' +
+            'VALUES ("'+add.modID+'", "'+groupingData[i][0]+'", "'+groupingData[i][3]+'", "" , "'+groupingData[i][1]+'", "'+groupingData[i][2]+'")'
         connection.query(queryInsertProjectInfo, function (error, results) {
             if (error) {
                 console.log(error)
@@ -430,56 +417,78 @@ web.post('/modifyModule', function(req, res){
     } else {
         res.redirect('/login')
     }
-    var modify = req.body
-    // var stuPath = "./table/"+modify.stuTable
-    // var groupPath = "./table/"+modify.groupTable
-    //
-    // var stuSheetList = xlsx.parse(stuPath)
-    // var stuData = stuSheetList[0].data
-    // var groupSheetList = xlsx.parse(groupPath)
-    // var groupData = groupSheetList[0].data
+
+    var update = req.body
+
+    var groupPath = "/public/table/"+update.groupingTable
+    var groupingSheetList = xlsx.parse(groupPath)
+    var groupingData = groupingSheetList[0].data
+
+    //calculate how many members in one team and the members' columns in the table
+    var groupingHeader = groupingData[0]
+    var membersInTeam = 0
+    var indexArray = []
+    for (var n = 0; n < groupingHeader.length; n++) {
+        if (groupingHeader[n].indexOf("member") != -1) {
+            indexArray[membersInTeam] = n
+            membersInTeam += 1
+        }
+    }
 
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'username',
         password: 'password',
-        database: 'traffic_feedback_system'
+        database: 'feedbackSystem'
     })
     connection.connect()
 
-    // for (var i = 1; i < stuData.length; i++) {
-    //     var queryInsertStu = 'UPDATE `student` SET `ID`="'+stuData[i][0]+'" `Name`="'+stuData[i][1]+'" `Role`="'+stuData[i][2]+
-    //         '" `Department`="'+stuData[i][3]+'" `Email`="'+stuData[i][4]+'" `Phone`="'+stuData[i][5]+'" `Module`="'+modify.modID+
-    //         '" `Group`="'+stuData[i][7]+' `Project`="'+stuData[i][8]+'" `Feedback1`="'+stuData[i][9]+'" `Feedback2`="'+stuData[i][10]+
-    //         '" `Feedback3`="'+stuData[i][11]+'" `Feedback4`="'+stuData[i][12]+'" `Feedback5`="'+stuData[i][13]+
-    //         '" WHERE `moduleID`="'+modify.modID+'" and `teamNumber`="' + groupData[i][1]+'"'
-    //     connection.query(queryInsertStu, function (error, results) {
-    //         if (error) {
-    //             console.log(error)
-    //         }
-    //     })
-    // }
-    //
-    // for (var j = 1; j < groupData.length; j++) {
-    //     var queryInsertGroup = 'UPDATE `grouping` SET `Project`="'+ groupData[j][2]+
-    //         '" `tm1`="'+groupData[j][3]+'" `tm2`"'+groupData[j][4]+'" `tm3`="'+groupData[j][5]+'" `Feedback1`="'+groupData[j][6]+
-    //         '" `Feedback2`="'+groupData[j][7]+'" `Feedback3`="'+groupData[j][8]+'" `Feedback4`="'+groupData[j][9]+
-    //         '" `Feedback5`="'+groupData[j][10]+'" WHERE `moduleID`="'+modify.modID+'" and `teamNumber`="'+groupData[j][1]+'"'
-    //     connection.query(queryInsertGroup, function (error, results) {
-    //         if (error) {
-    //             console.log(error)
-    //         }
-    //     })
-    // }
-
-    var queryInsert = 'UPDATE `module` SET `description`="'+modify.modDes+' `plan`="'+modify.modPlan+'" WHERE `moduleID`="'+modify.modID+'"'
-    connection.query(queryInsert, function (error, results) {
+    // Update Module table
+    var queryUpdateModule = 'UPDATE `Module` SET `moduleName`="'+update.modName+'", `moduleDescription`="'+update.modDes+'", ' +
+        '`modulePlan`="'+update.modPlan+'", `csvPath`="'+update.groupingTable+'" WHERE `moduleCode`="'+update.modID+'"'
+    connection.query(queryUpdateModule, function (error, results) {
         if (error) {
             console.log(error)
         }
     })
-    connection.end()
-    res.redirect('/')
+
+    // Delete & insert ProjectInfo table
+    var queryDeleteProjectInfo = 'DELETE FROM `ProjectInfo` WHERE `moduleCode`="'+update.modID+'"'
+    connection.query(queryDeleteProjectInfo, function (error, results) {
+        if (error) {
+            console.log(error)
+        }
+    })
+    for (var i = 1; i < groupingData.length; i++) {
+        var queryInsertProjectInfo = 'INSERT INTO `ProjectInfo` (`moduleCode`, `teamNumber`, `taStudentSPR`, `labCode`, `projectTitle`, `projectBrief`) ' +
+            'VALUES ("'+update.modID+'", "'+groupingData[i][0]+'", "'+groupingData[i][3]+'", "" , "'+groupingData[i][1]+'", "'+groupingData[i][2]+'")'
+        connection.query(queryInsertProjectInfo, function (error, results) {
+            if (error) {
+                console.log(error)
+            }
+        })
+    }
+
+    // Delete & insert ModStuTe table
+    var queryDeleteModStuTe = 'DELETE FROM `ModStuTe` WHERE `moduleCode`="'+update.modID+'"'
+    connection.query(queryDeleteModStuTe, function (error, results) {
+        if (error) {
+            console.log(error)
+        }
+    })
+    for (i = 1; i < groupingData.length; i++) {
+        for (var j = 0; j < membersInTeam; j++) {
+            var queryInsertModStuTe = 'INSERT INTO `ModStuTe`(`studentSPR`, `moduleCode`, `teamNumber`, `memberIndex`) ' +
+                'VALUES ("'+groupingData[i][indexArray[j]]+'", "'+add.modID+'", "'+groupingData[i][0]+'", "'+(j+1)+'")'
+            connection.query(queryInsertModStuTe, function (error, results) {
+                if (error) {
+                    console.log(error)
+                }
+            })
+        }
+    }
+
+    res.redirect('lecturerHomepage')
 })
 
 web.get('/admin', function(req, res){
