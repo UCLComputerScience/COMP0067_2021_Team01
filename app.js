40c45105f0c9b219de41c0e439eb08fb2f32a90e
Wang-Jade-729 <zczlaae@ucl.ac.uk>
2021-04-23 02:28:06 +0100

    .

---
+++ app.js
@@ -0,0 +1,117 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+const msal = require('@azure/msal-node');
+
+var favicon = require('serve-favicon');
+
+
+var bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var auth = require('./routes/auth');
+
+
+var app = express();
+
+// <MsalInitSnippet>
+// In-memory storage of logged-in users
+// For demo purposes only, production apps should store
+// this in a reliable storage
+app.locals.users = {};
+
+// MSAL config
+const msalConfig = {
+  auth: {
+    clientId: process.env.OAUTH_APP_ID,
+    authority: process.env.OAUTH_AUTHORITY,
+    clientSecret: process.env.OAUTH_APP_SECRET
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(loglevel, message, containsPii) {
+        console.log(message);
+      },
+      piiLoggingEnabled: false,
+      logLevel: msal.LogLevel.Verbose,
+    }
+  }
+};
+
+// Create msal application object
+app.locals.msalClient = new msal.ConfidentialClientApplication(msalConfig);
+
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+app.use(flash());
+// view engine setup
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function (req, res, next) {
+  // Read any flashed errors and save
+  // in the response locals
+  res.locals.error = req.flash('error_msg');
+
+  // Check for simple error string and
+  // convert to layout's expected format
+  var errs = req.flash('error');
+  for (var i in errs) {
+    res.locals.error.push({ message: 'An error occurred', debug: errs[i] });
+  }
+
+  // Check for an authenticated user and load
+  // into response locals
+  if (req.session.userId) {
+    res.locals.user = app.locals.users[req.session.userId];
+  }
+
+  next();
+});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+var hbs = require('hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/auth', auth);
+
+app.engine('html', require('express-art-template'))
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
