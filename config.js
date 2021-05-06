4634c3e257ec942ff34c87c92adc5163288830eb
Hu Weicong <zczlwh3@ucl.ac.uk>
2021-05-06 21:47:53 +0800

    final version

---
+++ config.js
@@ -0,0 +1,20 @@
+// const DB = {
+//     host:"localhost",
+//     port:3306,
+//     user:"root",
+//     password:"123456",
+//     database:"blog"
+// };
+
+const DB = {
+    host: 'comp0067.mysql.database.azure.com',
+    port: 3306,
+    user: 'azureuser@comp0067',
+    password: '981604108HZWws',
+    database: 'feedback',
+    ssl: true
+};
+
+
+module.exports = DB;
+
